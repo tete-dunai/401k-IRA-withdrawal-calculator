69ce8386fa00e0cd2c53c58f9fbc4b0ab20fbb8c
tete-dunai <121575041+tete-dunai@users.noreply.github.com>
2025-11-22 17:36:23 +0530

    Fix formatting of ResultsContainer component

--- src/components/ResultsContainer.tsx
+++ src/components/ResultsContainer.tsx
@@ -16,10 +16,10 @@ type ResultsContainerProps = {
 const ResultsContainer: React.FC<ResultsContainerProps> = ({ title, data }) => {
   return (
     <div className="bg-green-50 rounded-[16px] p-4 pb-4 pt-3 mt-10 mx-auto max-w-3xl border border-green-200 shadow-[0_0_10px_2px_#9ae6b4]">
-      <div className="text-lg font-medium text-gray-900 text-center mb-3">{title}</div>
       <p className="text-sm text-[#213547] mb-4 text-center">
         Disclaimer: These are estimated trial values. Please consult experts to derive accurate calculations.
       </p>
+      <div className="text-lg font-medium text-gray-900 text-center mb-3">{title}</div>
       <div className="grid grid-cols-3 text-center gap-1">
         <div>
           <div className="text-sm font-medium">USA Tax</div>
